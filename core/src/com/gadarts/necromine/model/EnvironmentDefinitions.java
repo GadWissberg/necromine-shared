230171178c5b5eef30832044920864a765f5cc5c
Gad Wissberg <gadw17@gmail.com>
2021-01-29 12:37:31 +0200

    Added MapNodesTypes.

--- core/src/com/gadarts/necromine/model/EnvironmentDefinitions.java
+++ core/src/com/gadarts/necromine/model/EnvironmentDefinitions.java
@@ -1,41 +1,60 @@
 package com.gadarts.necromine.model;
 
+import com.badlogic.gdx.math.Vector3;
 import com.gadarts.necromine.assets.Assets;
+import lombok.AccessLevel;
 import lombok.Getter;
 
 @Getter
 public enum EnvironmentDefinitions implements ElementDefinition {
-    MINE_WALL_1(Assets.Models.WALL_1, 4, 1, "Mine Wall 4x1"),
-    MINE_WALL_2(Assets.Models.WALL_2, 2, 1, "Mine Wall 2x1"),
-    CAVE_SUPPORTER_1(Assets.Models.CAVE_SUPPORTER_1, 1, 1, "Mine Wall Supporter #1"),
-    CAVE_SUPPORTER_2(Assets.Models.CAVE_SUPPORTER_2, 1, 1, "Mine Wall Supporter #2"),
-    CAVE_SUPPORTER_3(Assets.Models.CAVE_SUPPORTER_3, 1, 1, "Mine Wall Supporter #3"),
-    PILLAR(Assets.Models.PILLAR, 1, 1, "Pillar", true);
+	MINE_WALL_1(Assets.Models.WALL_1, 4, 1, "Mine Wall 4x1", new Vector3(0, 0, 1f), MapNodesTypes.OBSTACLE_KEY_DIAGONAL_FORBIDDEN),
+	MINE_WALL_2(Assets.Models.WALL_2, 2, 1, "Mine Wall 2x1", new Vector3(0, 0, 1f), MapNodesTypes.OBSTACLE_KEY_DIAGONAL_FORBIDDEN),
+	CAVE_SUPPORTER_1(Assets.Models.CAVE_SUPPORTER_1, 1, 1, "Mine Wall Supporter #1", false, new Vector3(0.5f, 0, 0.5f), MapNodesTypes.PASSABLE_NODE),
+	CAVE_SUPPORTER_2(Assets.Models.CAVE_SUPPORTER_2, 1, 1, "Mine Wall Supporter #2", false, new Vector3(0.5f, 0, 0.5f), MapNodesTypes.PASSABLE_NODE),
+	CAVE_SUPPORTER_3(Assets.Models.CAVE_SUPPORTER_3, 1, 1, "Mine Wall Supporter #3", false, new Vector3(0.5f, 0, 0.5f), MapNodesTypes.PASSABLE_NODE),
+	PILLAR(Assets.Models.PILLAR, 1, 1, "Pillar", true, new Vector3(0.5f, 0, 0.5f), MapNodesTypes.OBSTACLE_KEY_DIAGONAL_ALLOWED);
 
-    private final Assets.Models model;
-    private final int width;
-    private final int height;
-    private final String displayName;
-    private final boolean castShadow;
+	@Getter(AccessLevel.NONE)
+	private final Vector3 offset;
 
-    EnvironmentDefinitions(final Assets.Models model, final int width, final int height, final String displayName) {
-        this(model, width, height, displayName, false);
-    }
+	private final Assets.Models model;
+	private final int width;
+	private final int height;
+	private final String displayName;
+	private final boolean castShadow;
+	private final MapNodesTypes nodeType;
 
-    EnvironmentDefinitions(final Assets.Models model,
-                           final int width,
-                           final int height,
-                           final String displayName,
-                           final boolean castShadow) {
-        this.model = model;
-        this.width = width;
-        this.height = height;
-        this.displayName = displayName;
-        this.castShadow = castShadow;
-    }
 
-    @Override
-    public String getDisplayName() {
-        return displayName;
-    }
+	EnvironmentDefinitions(final Assets.Models model,
+						   final int width,
+						   final int height,
+						   final String displayName,
+						   final Vector3 offset,
+						   final MapNodesTypes nodeType) {
+		this(model, width, height, displayName, false, offset, nodeType);
+	}
+
+	EnvironmentDefinitions(final Assets.Models model,
+						   final int width,
+						   final int height,
+						   final String displayName,
+						   final boolean castShadow,
+						   final Vector3 offset, MapNodesTypes nodeType) {
+		this.model = model;
+		this.width = width;
+		this.height = height;
+		this.displayName = displayName;
+		this.castShadow = castShadow;
+		this.offset = offset;
+		this.nodeType = nodeType;
+	}
+
+	public Vector3 getOffset(final Vector3 output) {
+		return output.set(offset);
+	}
+
+	@Override
+	public String getDisplayName() {
+		return displayName;
+	}
 }
