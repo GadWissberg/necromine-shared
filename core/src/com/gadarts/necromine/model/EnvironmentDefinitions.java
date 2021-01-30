2f48ac2ea932f193fad7fb0e8a2da677039e6497
Gad Wissberg <gadw17@gmail.com>
2021-01-30 21:23:23 +0200

    Fixed offsets.

--- core/src/com/gadarts/necromine/model/EnvironmentDefinitions.java
+++ core/src/com/gadarts/necromine/model/EnvironmentDefinitions.java
@@ -7,11 +7,11 @@ import lombok.Getter;
 
 @Getter
 public enum EnvironmentDefinitions implements ElementDefinition {
-	MINE_WALL_1(Assets.Models.WALL_1, 4, 1, "Mine Wall 4x1", new Vector3(0, 0, 0.5f), MapNodesTypes.OBSTACLE_KEY_DIAGONAL_FORBIDDEN),
-	MINE_WALL_2(Assets.Models.WALL_2, 2, 1, "Mine Wall 2x1", new Vector3(0, 0, 0.5f), MapNodesTypes.OBSTACLE_KEY_DIAGONAL_FORBIDDEN),
-	CAVE_SUPPORTER_1(Assets.Models.CAVE_SUPPORTER_1, 1, 1, "Mine Wall Supporter #1", false, new Vector3(0, 0, 0.5f), MapNodesTypes.PASSABLE_NODE),
-	CAVE_SUPPORTER_2(Assets.Models.CAVE_SUPPORTER_2, 1, 1, "Mine Wall Supporter #2", false, new Vector3(0, 0, 0.5f), MapNodesTypes.PASSABLE_NODE),
-	CAVE_SUPPORTER_3(Assets.Models.CAVE_SUPPORTER_3, 1, 1, "Mine Wall Supporter #3", false, new Vector3(0, 0, 0.5f), MapNodesTypes.PASSABLE_NODE),
+	MINE_WALL_1(Assets.Models.WALL_1, 1, 4, "Mine Wall 4x1", new Vector3(0.5f, 0, 0), MapNodesTypes.OBSTACLE_KEY_DIAGONAL_FORBIDDEN),
+	MINE_WALL_2(Assets.Models.WALL_2, 1, 2, "Mine Wall 2x1", new Vector3(0.5f, 0, 0), MapNodesTypes.OBSTACLE_KEY_DIAGONAL_FORBIDDEN),
+	CAVE_SUPPORTER_1(Assets.Models.CAVE_SUPPORTER_1, 1, 1, "Mine Wall Supporter #1", false, new Vector3(0.5f, 0, 0), MapNodesTypes.PASSABLE_NODE),
+	CAVE_SUPPORTER_2(Assets.Models.CAVE_SUPPORTER_2, 1, 1, "Mine Wall Supporter #2", false, new Vector3(0.5f, 0, 0), MapNodesTypes.PASSABLE_NODE),
+	CAVE_SUPPORTER_3(Assets.Models.CAVE_SUPPORTER_3, 1, 1, "Mine Wall Supporter #3", false, new Vector3(0.5f, 0, 0), MapNodesTypes.PASSABLE_NODE),
 	PILLAR(Assets.Models.PILLAR, 1, 1, "Pillar", true, Vector3.Zero, MapNodesTypes.OBSTACLE_KEY_DIAGONAL_ALLOWED);
 
 	@Getter(AccessLevel.NONE)
