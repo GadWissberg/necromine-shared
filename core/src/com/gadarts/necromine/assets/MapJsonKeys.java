595a74045659730958a1f88b4d1f59cd9eb9227a
Gad Wissberg <gadw17@gmail.com>
2021-03-01 23:37:18 +0200

    Added json keys.

--- core/src/com/gadarts/necromine/assets/MapJsonKeys.java
+++ core/src/com/gadarts/necromine/assets/MapJsonKeys.java
@@ -1,14 +1,20 @@
 package com.gadarts.necromine.assets;
 
 public final class MapJsonKeys {
-	public static final String KEY_TARGET = "target";
-	public static final String KEY_TILES = "tiles";
-	public static final String KEY_WIDTH = "width";
-	public static final String KEY_DEPTH = "depth";
-	public static final String KEY_MATRIX = "matrix";
-	public static final String KEY_ROW = "row";
-	public static final String KEY_COL = "col";
-	public static final String KEY_DIRECTION = "direction";
-	public static final String KEY_CHARACTERS = "characters";
-	public static final String KEY_TYPE = "type";
+	public static final String TARGET = "target";
+	public static final String TILES = "tiles";
+	public static final String WIDTH = "width";
+	public static final String DEPTH = "depth";
+	public static final String HEIGHT = "height";
+	public static final String MATRIX = "matrix";
+	public static final String ROW = "row";
+	public static final String COL = "col";
+	public static final String DIRECTION = "direction";
+	public static final String CHARACTERS = "characters";
+	public static final String TYPE = "type";
+	public static final String EAST = "east";
+	public static final String SOUTH = "south";
+	public static final String WEST = "west";
+	public static final String NORTH = "north";
+	public static final String HEIGHTS = "heights";
 }
