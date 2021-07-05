14bc0518cb22cd5d191a2dcb005ef1c739d5ce48
Gad Wissberg <gadw17@gmail.com>
2021-07-05 23:30:08 +0300

    Working on enemy primary attack.

--- core/src/com/gadarts/necromine/model/characters/EnemyWeaponsDefinitions.java
+++ core/src/com/gadarts/necromine/model/characters/EnemyWeaponsDefinitions.java
@@ -1,14 +1,14 @@
 package com.gadarts.necromine.model.characters;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum EnemyWeaponsDefinitions {
-	ENERGY_BALL(0.1F);
+	ENERGY_BALL(0.1F, new Integer[]{1, 1, 1, 2, 2});
 
 	private final float frameDuration;
+	private final Integer[] damagePoints;
 
-	EnemyWeaponsDefinitions(float frameDuration) {
-		this.frameDuration = frameDuration;
-	}
 }
