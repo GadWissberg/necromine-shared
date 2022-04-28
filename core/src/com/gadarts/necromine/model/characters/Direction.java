e6334642c57353cc9a2499faa16716d674c1f4ee
gad.wissberg <gad.wissberg@viaccess-orca.com>
2022-04-28 14:39:15 +0300

    Added masks to the directions.

--- core/src/com/gadarts/necromine/model/characters/Direction.java
+++ core/src/com/gadarts/necromine/model/characters/Direction.java
@@ -1,28 +1,33 @@
 package com.gadarts.necromine.model.characters;
 
 import com.badlogic.gdx.math.Vector2;
+import lombok.Getter;
 
 public enum Direction {
-	SOUTH(0, 1),
-	SOUTH_EAST(1, 1),
-	EAST(1, 0),
-	NORTH_EAST(1, -1),
-	NORTH(0, -1),
-	NORTH_WEST(-1, -1),
-	WEST(-1, 0),
-	SOUTH_WEST(-1, 1);
+	SOUTH(0, 1, 0B00000010),
+	SOUTH_EAST(1, 1, 0B00000001),
+	EAST(1, 0, 0B00001000),
+	NORTH_EAST(1, -1, 0B001000000),
+	NORTH(0, -1, 0B010000000),
+	NORTH_WEST(-1, -1, 0B100000000),
+	WEST(-1, 0, 0B000100000),
+	SOUTH_WEST(-1, 1, 0B00000100);
 
 	private static final float DIR_ANGLE_SIZE = 45;
 	private final Vector2 directionVector;
 	private final float bottomBound;
 	private final float upperBound;
 
-	Direction(final int x, final int z) {
+	@Getter
+	private final int mask;
+
+	Direction(final int x, final int z, int mask) {
 		directionVector = new Vector2(x, z).nor();
 		float angleDeg = directionVector.angleDeg();
 		Vector2 auxVector = new Vector2();
 		bottomBound = auxVector.set(1, 0).setAngleDeg(angleDeg - DIR_ANGLE_SIZE / 2).angleDeg();
 		upperBound = angleDeg + DIR_ANGLE_SIZE / 2;
+		this.mask = mask;
 	}
 
 	public static Direction findDirection(final Vector2 direction) {
